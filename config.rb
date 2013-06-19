d31ab72e7dfd67a2100080fbd111a5a97a4ec3ef
david <backhausen@deback.de>
2013-06-19 16:52:02 +0200

    add compass/sass

---
+++ config.rb
@@ -0,0 +1,6 @@
+# für die entwicklung: compass watch {pfad zum ordner mit der config.rb}
+css_dir = "play1app/public/stylesheets"
+sass_dir = "play1app/sass"
+images_dir = "img"
+relative_assets = true
+asset_cache_buster :none
